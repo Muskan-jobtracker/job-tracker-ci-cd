68c035a874edf7883ebab930c057e311483f179b
Muskan-jobtracker <lalwanimuskan116@gmail.com>
2026-09-18 20:00:25 +0530

    Update title from 'Job Tracker' to 'Job Tracker Application'

--- first.php
+++ first.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Job Tracker</title>
+    <title>Job Tracker Application</title>
 
     <style>
         * {
